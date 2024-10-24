0045287cd459ffe2653647679024acac2ac9f8dd
fatimazehra2408 <fatimazehra2408@gmail.com>
2024-10-24 21:18:03 +0530

    My first program

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+class Pattern
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Enter a number to print: ");
+		int i,j;
+		for(i=1;i<=3;i++)
+		{
+			for(j=1;j<=3;j++)
+				System.out.println("*");
+			System.out.println("\n");
+		}
+	}
+}
